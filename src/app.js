1800ed688a540b6e3a38ac910468d5f1c359c081
Lipe <lipezerafar@gmail.com>
2026-03-12 17:17:58 -0300

    feat: adiciona metodo get by id

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());//middleware: ter acesso a requisicoes e respostas, e fazer ações nelas - encadeia esses middlewars
+//requisicoes com body de json vai passar pelo middleware e parseado pra json - com body, chega como string
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Harry Potter"
+    },
+    {
+        id: 2,
+        titulo: "Jujutsu Kaisen"
+    }
+];
+
+function buscaLivros(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
+app.get("/", (req, res) =>{
+    res.status(200).send("Api Node.js");//send = dados mais simples
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);//params pq é um parametro da rota - id = nome do id
+    res.status(200).json(livros[index]);
+});
